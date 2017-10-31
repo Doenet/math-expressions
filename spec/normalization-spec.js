c8999cfcdee7d209fc9dba0110b67c87ae6ad96e
Duane Nykamp <nykamp@umn.edu>
2017-10-30 21:29:41 -0500

    Default order of factors and terms

--- spec/normalization-spec.js
+++ spec/normalization-spec.js
@@ -130,3 +130,96 @@ describe("normalize tuples", function() {
     
 });
 
+
+describe("default order", function () {
+
+    it("order terms", function () {
+	expect(me.from("1-3+x-z").default_order().tree).toEqual(
+	    me.from("-z+1+x-3").default_order().tree);
+    });
+
+    it("order factors", function () {
+	expect(me.from("ajsdz").default_order().tree).toEqual(
+	    me.from("sazdj").default_order().tree);
+    });
+
+    it("order equality", function () {
+	expect(me.from("xy=ab").default_order().tree).toEqual(
+	    me.from("ab=xy").default_order().tree);
+	expect(me.from("y=xy=x+y").default_order().tree).toEqual(
+	    me.from("y+x=yx=y").default_order().tree);
+    });
+
+    it("normalizes negatives in factors", function () {
+	var expr1 = me.from([ '+', 1, [ '*', [ '-', 'x' ], 'y' ] ]);
+	var expr2 = me.from([ '+', 1, [ '-', [ '*', 'x', 'y' ] ] ]);
+	expect(expr1.default_order().tree).toEqual(expr2.default_order().tree);
+    });
+
+    it("removes multiple negatives", function () {
+	expect(me.from("3--x").default_order().tree).toEqual(
+	    me.from("3+x").default_order().tree);
+	expect(me.from("3---x").default_order().tree).toEqual(
+	    me.from("3-x").default_order().tree);
+    });
+
+    it("normalize negative combination", function () {
+	expect(me.from('5+x(-3)').default_order().tree).toEqual(
+	    me.from("5-3x").default_order().tree);
+	expect(me.from('5-x(-3)').default_order().tree).toEqual(
+	    me.from("5+3x").default_order().tree);
+	expect(me.from('5-x(--3)').default_order().tree).toEqual(
+	    me.from("5-3x").default_order().tree);
+	expect(me.from('5--x(-3)').default_order().tree).toEqual(
+	    me.from("5-3x").default_order().tree);
+	expect(me.from('5--x(--3)').default_order().tree).toEqual(
+	    me.from("5+3x").default_order().tree);
+    });
+    
+    it("order expression", function () {
+	expect(me.from("(x-2y)*(sin(yz)-3+u)v-3/z").default_order().tree).toEqual(
+	    me.from("-3/z+(u-3+sin(zy))(-y*2+x)v").default_order().tree);
+    });
+    
+
+    it("order equalities and inequalities", function () {
+	expect(me.from("abc=x+y=-1/y").default_order().tree).toEqual(
+	    me.from("y+x=-1/y=cab").default_order().tree);
+
+	expect(me.from("8/sin(xy) != e^(b-a)").default_order().tree).toEqual(
+	    me.from("e^(-a+b) != 8/sin(yx)").default_order().tree);
+	
+    });
+
+    it("order logicals", function () {
+	expect(me.from("(A or B) and (C or D)").default_order().tree).toEqual(
+	    me.from("(D or C) and (B or A)").default_order().tree);
+
+    });
+
+    it("order set operations", function () {
+	expect(me.from("(A union B) intersect (C union D)").default_order().tree).toEqual(
+	    me.from("(D union C) intersect (B union A)").default_order().tree);
+
+    });
+
+    it("order inequalities", function () {
+	expect(me.from("a >= b or b < d").default_order().tree).toEqual(
+	    me.from("d > b or b <= a").default_order().tree);
+
+	expect(me.from("a >= b >= c > d").default_order().tree).toEqual(
+	    me.from("d < c <=b <= a").default_order().tree);
+	
+    });
+
+    it("order containment", function () {
+	expect(me.from("a elementof A and b notelementof B").default_order().tree).toEqual(
+	    me.from("B notcontainselement b and A containselement a").default_order().tree);
+
+	expect(me.from("A superset B or C notsubset D").default_order().tree).toEqual(
+	    me.from("D notsuperset C or B subset A").default_order().tree);
+	
+    });
+
+    
+});
--- spec/transformation-spec.js
+++ spec/transformation-spec.js
@@ -1,10 +1,11 @@
 var me = require('../lib/math-expressions');
+var trees = require('../lib/trees');
 
 describe("expand factors", function () {
 
     it("expand polynomial", function () {
 
-	expect(me.fromText("(a+x)(b-y)").expand().equals(me.fromText("ab-ay+xb-xy"))).toBeTruthy();
+	expect(trees.equal(me.fromText("(a+x)(b-y)").expand().tree, me.fromText("ab-ay+xb-xy").tree)).toBeTruthy();
 
     });
 
@@ -13,10 +14,13 @@ describe("expand factors", function () {
 	var factored = me.fromText("x(sin(x)-cos(y))(3log(z)+be^a)(ts+q^2)");
 	var expanded = me.fromText("x sin(x) 3 log(z) ts + x sin(x) 3 log(z) q^2 + x sin(x) be^a ts + x sin(x) be^a q^2  - x cos(y) 3 log(z) ts - x cos(y) 3 log(z) q^2 - x cos(y) be^a ts - x cos(y) be^a q^2");
 	var factored_expanded = factored.expand();
-	console.log("About to check equality of complicated expression");
-	expect(factored_expanded.equals(expanded)).toBeTruthy();
-	console.log("Finished checking equality of complicated expression");
+	expect(trees.equal(factored_expanded.tree,expanded.tree)).toBeTruthy();
 
+    });
+
+    it("expand negative", function () {
+	expect(trees.equal(me.fromText('-(x+y)').expand().tree,
+			   me.fromText('-x-y').tree)).toBeTruthy();
     });
 
 });
