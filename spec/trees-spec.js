6f26f76cd19b88425cb652a9ebcb5a14dc4fa0c6
Duane Nykamp <nykamp@umn.edu>
2017-11-02 18:08:54 -0500

    Match trees with constraints, allowing permutations, and implicit identities.

--- spec/trees-spec.js
+++ spec/trees-spec.js
@@ -5,7 +5,7 @@ function TREE(s) {
     return Expression.fromText(s).tree;
 }
 
-describe("via trees", function() {
+describe("tree basics", function() {
     it("cos x == cos x", function() {
 	expect(trees.equal( TREE('cos x'), TREE('cos x') )).toBeTruthy();
     });
@@ -44,6 +44,10 @@ describe("via trees", function() {
 			    TREE('(y+1)*(x+2)/((y+1)^(x+2))') )).toBeTruthy();
     });
 
+
+});
+
+describe("tree matching", function () {
     it("x+y matches the pattern a+b", function() {
 	expect(trees.match( TREE('x+y'), TREE('a+b') )).toEqual( {a:'x', b:'y'} );
     });
@@ -52,16 +56,200 @@ describe("via trees", function() {
 	expect(trees.match( TREE('x+y'), TREE('a*b') )).toBeFalsy();
     });
 
-
     it("match must match entire tree", function() {
 	expect(trees.match( TREE('x+y/z'), TREE('a/b') )).toBeFalsy();
     });
+
+    it("match must be consistent", function () {
+	var target1 = TREE('x+y/z');
+	var target2 = TREE('x+y/x');
+	expect(trees.match( target1, TREE('a+b/c'))).toBeTruthy();
+	expect(trees.match( target1, TREE('a+b/a'))).toBeFalsy();
+	expect(trees.match( target2, TREE('a+b/a'))).toBeTruthy();
+    });	
+
+    it("by default even multicharacter patterns are placeholders", function () {
+	expect(trees.match( ['+', 'x', 'y'], ['+', 'a', 'bc'])).toBeTruthy();
+	expect(trees.match( ['+', 'x', 'bc'], ['+', 'a', 'bc'])).toBeTruthy();
+    });
+
+    it("numbers match exactly", function () {
+	expect(trees.match( TREE('3x+5'), TREE('ab+5'))).toBeTruthy();
+	expect(trees.match( TREE('3x+5'), TREE('ab+6'))).toBeFalsy();
+    });
+
+    it("addition matches subtraction but not vice-versa", function () {
+	expect(trees.match( TREE('x-y'), TREE('a+b'))).toBeTruthy();
+	expect(trees.match( TREE('x+y'), TREE('a-b'))).toBeFalsy();
+    });
+
+    if("specify variables", function () {
+	expect(trees.match( TREE('x+y'), TREE('a+b'))).toBeTruthy();
+	expect(trees.match( TREE('x+y'), TREE('a+b')), {a: true, b: true}).toBeTruthy();
+	expect(trees.match( TREE('x+y'), TREE('a+b')), {a: true}).toBeFalsy();
+	expect(trees.match( TREE('x+y'), TREE('a+b')), {b: true}).toBeFalsy();
+    });
+    
+    it("matching with function conditions", function () {
+	var pattern = TREE('a+b');
+	function isString(s) { return (typeof s === 'string');}
+	function isNumber(s) { return (typeof s === 'number');}
+
+	expect(trees.match( TREE('2x+y'), pattern)).toBeTruthy();
+	expect(trees.match( TREE('2x+y'), pattern, {a: true, b: isString})).toBeTruthy();
+	expect(trees.match( TREE('x+2y'), pattern)).toBeTruthy();
+ 	expect(trees.match( TREE('x+2y'), pattern, {a: true, b: isString})).toBeFalsy();
+
+	expect(trees.match( TREE('x+2'), pattern, {a: isString, b: isNumber})).toBeTruthy();
+	expect(trees.match( TREE('x+2'), pattern, {b: isString, a: isNumber})).toBeFalsy();
+
+    });
+
+    it("matching with regular expression conditions", function () {
+	var pattern = TREE('a+b');
+
+	expect(trees.match( TREE('2x+3y'), pattern)).toBeTruthy();
+	expect(trees.match( TREE('2x+3y'), pattern, {a: true, b: /y/})).toBeTruthy();
+	expect(trees.match( TREE('2x+3y'), pattern, {a: true, b: /^y$/})).toBeFalsy();
+	expect(trees.match( TREE('2x+3y'), pattern, {a: /.*/, b: /y/})).toBeTruthy();
+
+ 	expect(trees.match( TREE('2x+3y'), pattern, {a: true, b: /^[a-zA-Z]$/})).toBeFalsy();
+ 	expect(trees.match( TREE('2x+3y'), TREE('a+3b'), {a: true, b: /^[a-zA-Z]$/})).toBeTruthy();
+
+	expect(trees.match( TREE('x+2'), pattern, {a: /^[a-zA-Z]$/, b: /^\d+$/})).toBeTruthy();
+	expect(trees.match( TREE('x+2'), pattern, {b: /^[a-zA-Z]$/, a: /^\d+$/})).toBeFalsy();
+
+    });
+
+    it("invalid matching conditions fail gracefully", function () {
+
+	expect(trees.match( TREE('2x+y'),  TREE('a+b'), {a: false, b: 'h'})).toBeFalsy();
+
+    });
+
+    
+    it("match with permutation", function () {
+	var pattern = TREE('e^(ax^2+bx+c)');
+	function isNumber(s) { return (typeof s === 'number');}
+	
+	expect(trees.match( TREE('e^(0.3s^2+3s+7)'), pattern,
+			    { a: isNumber, b: isNumber, c: isNumber,
+			      x: /^[a-zA-Z]$/ }
+			  )).toBeTruthy();
+
+	expect(trees.match( TREE('e^(7+3s+s^2*0.3)'), pattern,
+			    { a: isNumber, b: isNumber, c: isNumber,
+			      x: /^[a-zA-Z]$/ }
+			  )).toBeFalsy();
+
+	expect(trees.match( TREE('e^(7+3s+s^2*0.3)'), pattern,
+			    { a: isNumber, b: isNumber, c: isNumber,
+			      x: /^[a-zA-Z]$/ },
+			    {allow_permutations: true}
+			  )).toBeTruthy();
+	
+    });
+    
+
+    it("match with implicit identity", function () {
+	var pattern = TREE('ax^2+bx+c');
+	
+	function isNumber(s) {
+	    if (typeof s === 'number')
+		return true;
+	    if (Array.isArray(s) && s[0] === '-' && (typeof s[1] === 'number'))
+		return true;
+	    return false;
+	}
+
+	var match = trees.match( TREE('0.3s^2-3s+7'), pattern,
+				 { a: isNumber, b: isNumber, c: isNumber,
+				   x: /^[a-zA-Z]$/ },
+				 {allow_permutations: true}
+			       );
+	
+	expect(match).toBeTruthy();
+	expect(match['a']==0.3 && trees.equal(match['b'],['-', 3])
+	       && match['c'] == 7).toBeTruthy();
+
+	expect(trees.match( TREE('s^2-3s+7'), pattern,
+			    { a: isNumber, b: isNumber, c: isNumber,
+			      x: /^[a-zA-Z]$/ },
+			    {allow_permutations: true}
+			  )).toBeFalsy();
+
+	match = trees.match( TREE('s^2-3s+7'), pattern,
+			     { a: isNumber, b: isNumber, c: isNumber,
+			       x: /^[a-zA-Z]$/ },
+			     { allow_permutations: true,
+			       allow_implicit_identities: ['a']}
+			   );
+	expect(match).toBeTruthy();
+	expect(match['a']==1 && trees.equal(match['b'],['-', 3])
+	       && match['c'] == 7).toBeTruthy();
+
+
+	expect(trees.match( TREE('s^2-3s'), pattern,
+			    { a: isNumber, b: isNumber, c: isNumber,
+			      x: /^[a-zA-Z]$/ },
+			    { allow_permutations: true,
+			      allow_implicit_identities: ['a']}
+			  )).toBeFalsy();
+	
+	match = trees.match( TREE('s^2-3s'), pattern,
+			     { a: isNumber, b: isNumber, c: isNumber,
+			      x: /^[a-zA-Z]$/ },
+			     { allow_permutations: true,
+			       allow_implicit_identities: ['a', 'c']}
+			   );
+	expect(match).toBeTruthy();
+	expect(match['a']==1 && trees.equal(match['b'],['-', 3])
+	       && match['c'] == 0).toBeTruthy();
+	
+       
+    });
+
+
+    it("consistency with with implicit identity", function () {
+	var pattern = TREE('bx^2+bx+c');
+
+	function isNumber(s) {
+	    if (typeof s === 'number')
+		return true;
+	    if (Array.isArray(s) && s[0] === '-' && (typeof s[1] === 'number'))
+		return true;
+	    return false;
+	}
+
+	expect(trees.match( TREE('s^2-3s+7'), pattern,
+			    { a: isNumber, b: isNumber, c: isNumber,
+			      x: /^[a-zA-Z]$/ },
+			    { allow_permutations: true,
+			      allow_implicit_identities: ['b']}
+			  )).toBeFalsy();
+	expect(trees.match( TREE('s^2+1s+7'), pattern,
+			    { a: isNumber, b: isNumber, c: isNumber,
+			      x: /^[a-zA-Z]$/ },
+			    { allow_permutations: true,
+			      allow_implicit_identities: ['b']}
+			  )).toBeTruthy();
+	expect(trees.match( TREE('s^2+s+7'), pattern,
+			    { a: isNumber, b: isNumber, c: isNumber,
+			      x: /^[a-zA-Z]$/ },
+			    { allow_permutations: true,
+			      allow_implicit_identities: ['b']}
+			  )).toBeTruthy();
+	
+    });
+	
+});
+
+describe("tree transformations", function () {
 
     it("replacing x*y in x*y+z with y/x results in y/x+z", function() {
 	var subtree = ['*', 'x', 'y'];
 	var replacement = ['/', 'y', 'x'];
 	var root = ['+', subtree, 'z'];
-	//var root = ['+', ['*', 'x', 'y'], 'z'];
 
 	expect(trees.equal( trees.replaceSubtree( root, subtree, replacement ),
 			    TREE('y/x + z') )).toBeTruthy();
