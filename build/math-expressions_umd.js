6f1ccc44277d44f05321fbd89341c34cdd5f0f71
Duane Nykamp <nykamp@umn.edu>
2019-07-02 07:23:03 -0500

    round to specified number of decimals

--- build/math-expressions_umd.js
+++ build/math-expressions_umd.js
@@ -77241,14 +77241,23 @@
       throw Error("For round_numbers_to_precision, digits must be positive");
     }
 
+    if(!Number.isFinite(digits)) {
+      throw Error("For round_numbers_to_precision, digits must be a number");
+    }
+
+    if(digits > 15) {
+      return tree;
+    }
+
+    digits = Math.round(digits);
+
     return round_numbers_to_precision_sub(tree,digits);
   }
 
   const round_numbers_to_precision_sub = function(tree, digits=14) {
-    if(digits > 15) {
-      return tree;
-    }
+
     if(typeof tree === "number") {
+
       if(Number.isFinite(tree) && tree !== 0) {
         const scaleFactor = math$19.floor(math$19.log10(math$19.abs(tree)));
         const n = digits - scaleFactor - 1;
@@ -77269,8 +77278,47 @@
     return [operator, ...operands.map(x => round_numbers_to_precision_sub(x,digits))]
   };
 
+
+  function round_numbers_to_decimals(expr_or_tree, ndecimals=14) {
+    // round any numbers to specified number of decimals
+
+    var tree = get_tree(expr_or_tree);
+
+
+    if(!Number.isFinite(ndecimals)) {
+      throw Error("For round_numbers_to_decimals, ndecimals must be a number");
+    }
+
+    ndecimals = Math.round(ndecimals);
+
+    // no need to go much beyond limits of double precision
+    ndecimals = Math.max(-330, Math.min(330, ndecimals));
+
+    return round_numbers_to_decimals_sub(tree,ndecimals);
+  }
+
+  const round_numbers_to_decimals_sub = function(tree, ndecimals=0) {
+
+    if(typeof tree === "number") {
+      if(ndecimals < 0) {
+        // mathjs toFixed truncates zeros when n is negative
+        // so add back on when creating float
+        return parseFloat(number_7(tree, ndecimals)+'0'.repeat(math$19.abs(ndecimals)));
+      } else {
+        return parseFloat(number_7(tree, ndecimals))
+      }
+    }
+    if(!Array.isArray(tree)) {
+      return tree;
+    }
+    let operator = tree[0];
+    let operands = tree.slice(1);
+    return [operator, ...operands.map(x => round_numbers_to_decimals_sub(x,ndecimals))]
+  };
+
   var round$2 = /*#__PURE__*/Object.freeze({
-    round_numbers_to_precision: round_numbers_to_precision
+    round_numbers_to_precision: round_numbers_to_precision,
+    round_numbers_to_decimals: round_numbers_to_decimals
   });
 
   const expression_to_tree = [
--- spec/quick_rounding.spec.js
+++ spec/quick_rounding.spec.js
@@ -74,3 +74,77 @@ describe("round to significant digits", function () {
 
 });
 
+
+describe("round to decimals", function () {
+
+  it("single number", function () {
+
+    let expr = me.fromText('1.234567890123456789');
+    expect(expr.round_numbers_to_decimals(100).tree).toEqual(1.2345678901234567)
+    expect(expr.round_numbers_to_decimals(13).tree).toEqual(1.2345678901235)
+    expect(expr.round_numbers_to_decimals(9).tree).toEqual(1.23456789)
+    expect(expr.round_numbers_to_decimals(3).tree).toEqual(1.235)
+
+    expr = me.fromText('12345678901234567890000000000');
+    expect(expr.round_numbers_to_decimals(100).tree).toEqual(12345678901234568000000000000)
+    expect(expr.round_numbers_to_decimals(-15).tree).toEqual(12345678901235000000000000000)
+    expect(expr.round_numbers_to_decimals(-19).tree).toEqual(12345678900000000000000000000)
+    expect(expr.round_numbers_to_decimals(-25).tree).toEqual(12350000000000000000000000000)
+
+    expr = me.fromText('0.000000000000000000001234567890123456789');
+    expect(expr.round_numbers_to_decimals(100).tree).toEqual(0.0000000000000000000012345678901234568)
+    expect(expr.round_numbers_to_decimals(34).tree).toEqual(0.0000000000000000000012345678901235)
+    expect(expr.round_numbers_to_decimals(30).tree).toEqual(0.00000000000000000000123456789)
+    expect(expr.round_numbers_to_decimals(24).tree).toEqual(0.000000000000000000001235)
+
+    expect(me.fromAst(0).round_numbers_to_decimals(10).tree).toEqual(0)
+    expect(me.fromAst(Infinity).round_numbers_to_decimals(10).tree).toEqual(Infinity)
+    expect(me.fromAst(-Infinity).round_numbers_to_decimals(10).tree).toEqual(-Infinity)
+
+  });
+
+  it("expression", function () {
+
+    let expr = me.fromText('exp(1.234567890123456789x+9876.543210987654321)/(5+8520203156.435345956432x)');
+    expect(expr.round_numbers_to_decimals(100).equals(
+      me.fromText('exp(1.2345678901234567 x + 9876.543210987654)/(5 + 8520203156.435346 x)'))).toBeTruthy();
+    expect(expr.round_numbers_to_decimals(13).equals(
+      me.fromText('exp(1.2345678901235 x + 9876.543210987654)/(5 + 8520203156.435346 x)'))).toBeTruthy();
+    expect(expr.round_numbers_to_decimals(9).equals(
+      me.fromText('exp(1.23456789 x + 9876.543210988)/(5 + 8520203156.435346 x)'))).toBeTruthy();
+    expect(expr.round_numbers_to_decimals(-2).equals(
+      me.fromText('exp(0 x + 9900)/(0 + 8520203200 x)'))).toBeTruthy();
+
+  });
+
+  it("don't round fractions", function () {
+
+    let expr = me.fromText('3123414232/72512351634x + 381439619649.253 y');
+    expect(expr.round_numbers_to_decimals(100).equals(
+      me.fromText('3123414232/72512351634x + 381439619649.253 y'))).toBeTruthy();
+    expect(expr.round_numbers_to_decimals(2).equals(
+      me.fromText('3123414232/72512351634x + 381439619649.25 y'))).toBeTruthy();
+    expect(expr.round_numbers_to_decimals(-2).equals(
+      me.fromText('3123414200/72512351600x + 381439619600 y'))).toBeTruthy();
+    expect(expr.round_numbers_to_decimals(-8).equals(
+      me.fromText('3100000000/72500000000x + 381400000000 y'))).toBeTruthy();
+
+  });
+
+
+  it("don't round pi or e", function () {
+
+    let expr = me.fromText('3123414232/72512351634e + 381439619649.253 pi');
+    expect(expr.round_numbers_to_decimals(100).equals(
+      me.fromText('3123414232/72512351634exp(1) + 381439619649.253 pi'))).toBeTruthy();
+    expect(expr.round_numbers_to_decimals(2).equals(
+      me.fromText('3123414232/72512351634exp(1) + 381439619649.25 pi'))).toBeTruthy();
+    expect(expr.round_numbers_to_decimals(-2).equals(
+      me.fromText('3123414200/72512351600exp(1) + 381439619600 pi'))).toBeTruthy();
+    expect(expr.round_numbers_to_decimals(-8).equals(
+      me.fromText('3100000000/72500000000exp(1) + 381400000000 pi'))).toBeTruthy();
+
+  });
+
+
+});
