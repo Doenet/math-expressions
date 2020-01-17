b426a73c89560e7d450e0dd397b8c6a8c029a20f
Jake Conway <jake.h.conway@gmail.com>
2020-01-16 19:55:45 -0500

    Fix demo page

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,20 @@
 // rollup.config.js
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import globals from 'rollup-plugin-node-globals';
 import builtins from 'rollup-plugin-node-builtins';
 
 export default {
   input: 'lib/math-expressions.js',
-  output: [{
-    file: 'build/math-expressions_umd.js',
+  output: {
+    file: 'build/math-expressions.js',
     format: 'umd',
     name: 'MathExpression'
-  },{
-    file: 'build/math-expressions.js',
-    format: 'es'
-  }],
+  },
   plugins: [
     resolve(),
     commonjs(),
+    globals(),
     builtins()
   ]
 };
